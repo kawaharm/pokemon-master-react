29159a8c260781f008f0fabcfdf76ac84550a130
kawaharm <mjbk295@gmail.com>
2023-02-21 20:14:05 -0800

    add materials ui css

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import BattleScreen from "./BattleScreen";
-import { Container } from "@mui/material";
+import { Container, Box } from "@mui/material";
 import "./App.css";
 
 const StartMenu = () => {
@@ -18,7 +18,21 @@ const StartMenu = () => {
   return (
     <Container maxWidth="lg">
       {!toggleBattleScreen && (
-        <div id="startScreen">
+        <Box
+          sx={{
+            display: "grid",
+            gridTemplateRows: "0.75fr, 0.25fr",
+            gap: 10,
+            height: 800,
+            width: 1000,
+            mx: "auto",
+            my: 2,
+            background:
+              "linear-gradient(-45deg, #49256d, #976dc1, #6c4a8e, #49256d)",
+            border: 3,
+            borderColor: "text.primary",
+          }}
+        >
           <div id="logo">
             <img src="images/game-logo.png" alt="game logo" />
           </div>
@@ -36,7 +50,7 @@ const StartMenu = () => {
               onClick={handleSubmit}
             />
           </div>
-        </div>
+        </Box>
       )}
       {toggleBattleScreen && <BattleScreen username={username} />}
     </Container>
