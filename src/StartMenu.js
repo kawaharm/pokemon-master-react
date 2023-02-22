9f00e2fad8ea73166fb96a7e97e1d6c61976e047
kawaharm <mjbk295@gmail.com>
2023-02-21 19:35:56 -0800

    add battle screen

--- src/StartMenu.js
+++ src/StartMenu.js
@@ -1,29 +1,45 @@
 import React, { useState } from "react";
 import BattleScreen from "./BattleScreen";
+import { Container } from "@mui/material";
 import "./App.css";
 
 const StartMenu = () => {
   const [toggleBattleScreen, setToggleBattleScreen] = useState(false);
+  const [username, setUsername] = useState("Pikachu");
 
   const handleSubmit = () => {
     setToggleBattleScreen(true);
   };
 
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
   return (
-    <div>
+    <Container maxWidth="lg">
       {!toggleBattleScreen && (
         <div id="startScreen">
           <div id="logo">
             <img src="images/game-logo.png" alt="game logo" />
           </div>
-          <form id="input-form" autocomplete="off" onSubmit={handleSubmit}>
-            <input id="input-text" type="text" placeholder="Enter Username" />
-            <input id="submit" type="submit" value="LET'S PLAY!" />
-          </form>
+          <div id="input-form">
+            <input
+              id="input-text"
+              type="text"
+              onChange={handleChange}
+              placeholder="Enter Username"
+            />
+            <input
+              id="submit"
+              type="button"
+              value="LET'S PLAY!"
+              onClick={handleSubmit}
+            />
+          </div>
         </div>
       )}
-      {toggleBattleScreen && <BattleScreen />}
-    </div>
+      {toggleBattleScreen && <BattleScreen username={username} />}
+    </Container>
   );
 };
 
